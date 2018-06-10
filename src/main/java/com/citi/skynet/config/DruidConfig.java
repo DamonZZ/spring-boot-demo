0a2fdd621f250ab6a2a82071db45530cbbcce1f8
damon <zjsgdtcm@sina.com>
2018-06-10 19:59:31 +0800

    durid decrypt
    replace of the hardcode pwd in properties

--- src/main/java/com/citi/skynet/config/DruidConfig.java
+++ src/main/java/com/citi/skynet/config/DruidConfig.java
@@ -1,11 +1,14 @@
 package com.citi.skynet.config;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.config.ConfigTools;
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
@@ -16,10 +19,9 @@ public class DruidConfig {
 
 	@ConfigurationProperties(prefix = "spring.druid")
 	@Bean(initMethod = "init", destroyMethod = "close")
-	public DruidDataSource dataSource() {
+	public DruidDataSource dataSource() throws Exception {
 		DruidDataSource dataSource = new DruidDataSource();
 		dataSource.setProxyFilters(Lists.newArrayList(statFilter()));
-		;
 		return dataSource;
 	}
 
